56711d90c4e62cb7d06c78d5aa605685e3a819c4
hoe-hunter <70718637+hoo-hunter@users.noreply.github.com>
2020-11-07 17:31:34 +0300

    FINAL VERSION

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Посчитать";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -48,9 +48,8 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(120, 155);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
             // 
             // textBox1
             // 
